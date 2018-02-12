4a4af24944e537502155e9a1a7194f6e41c2eb84
Topi T <smobey@gmail.com>
2018-02-12 14:28:55 +0200

    4.2

---
+++ models/blog.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema;
+
+const Blog = mongoose.model('Blog', {
+    title: String,
+    author: String,
+    url: String,
+    likes: Number
+  })
+
+module.exports = Blog
